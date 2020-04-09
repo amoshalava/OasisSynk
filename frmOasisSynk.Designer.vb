64a2b0d54264c3115bb7a4726e3ce153abcf9d45
Amos Halava <amoshalava@hotmail.com>
2020-04-09 16:54:01 -0400

    Add project files.

---
+++ frmOasisSynk.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmOasisSynk
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ListViewItem1 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem("amos")
+        Me.picbLogo = New System.Windows.Forms.PictureBox()
+        Me.lstvSynkList = New System.Windows.Forms.ListView()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        CType(Me.picbLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picbLogo
+        '
+        Me.picbLogo.BackgroundImage = Global.OasisSynk.My.Resources.Resources.Oasis_2
+        Me.picbLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.picbLogo.Location = New System.Drawing.Point(12, 12)
+        Me.picbLogo.Name = "picbLogo"
+        Me.picbLogo.Size = New System.Drawing.Size(309, 135)
+        Me.picbLogo.TabIndex = 0
+        Me.picbLogo.TabStop = False
+        '
+        'lstvSynkList
+        '
+        Me.lstvSynkList.HideSelection = False
+        Me.lstvSynkList.Items.AddRange(New System.Windows.Forms.ListViewItem() {ListViewItem1})
+        Me.lstvSynkList.Location = New System.Drawing.Point(12, 153)
+        Me.lstvSynkList.Name = "lstvSynkList"
+        Me.lstvSynkList.Size = New System.Drawing.Size(775, 217)
+        Me.lstvSynkList.TabIndex = 1
+        Me.lstvSynkList.UseCompatibleStateImageBehavior = False
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(740, 376)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(47, 43)
+        Me.btnAdd.TabIndex = 2
+        Me.btnAdd.Text = "+"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'frmOasisSynk
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(799, 433)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.lstvSynkList)
+        Me.Controls.Add(Me.picbLogo)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmOasisSynk"
+        Me.Text = "OasisSynk"
+        CType(Me.picbLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents picbLogo As PictureBox
+    Friend WithEvents lstvSynkList As ListView
+    Friend WithEvents btnAdd As Button
+End Class
---
+++ frmOasisSynk.vb
@@ -0,0 +1,131 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+Imports System.Runtime.Serialization
+Imports System.Runtime.Serialization.Formatters.Binary
+
+
+Public Class frmOasisSynk
+
+    <Serializable>
+    Public Class SynkItems
+        Public myList As List(Of SynkItemSQLDatabase)
+
+        Public Sub New()
+            myList = New List(Of SynkItemSQLDatabase)
+        End Sub
+
+        Public Sub Add(svr As SynkItemSQLDatabase)
+            myList.Add(svr)
+        End Sub
+
+        ' no reason it cant also create server objects for you
+        Public Sub Add(sname As String, uname As String, pw As String)
+            myList.Add(New SynkItemSQLDatabase With {.Server = sname, .Username = uname, .Password = pw})
+        End Sub
+
+        'toDo Contains, Count, Item etc as needed
+
+        Public Sub Save(mypath As String)
+            Using fs As New FileStream(mypath, FileMode.OpenOrCreate)
+                Dim bf As New BinaryFormatter
+                bf.Serialize(fs, myList)
+            End Using
+        End Sub
+
+        Public Function Load(mypath As String) As Int32
+            'ToDo: check if file exists
+            Try
+
+                Using fs As New FileStream(mypath, FileMode.Open)
+                    Dim bf As New BinaryFormatter
+                    myList = CType(bf.Deserialize(fs), List(Of SynkItemSQLDatabase))
+                End Using
+
+                If myList IsNot Nothing Then
+                    Return myList.Count
+                Else
+                    Return 0
+                End If
+
+            Catch ex As Exception
+                Console.WriteLine("ERROR: " & ex.ToString)
+                Return 0
+            End Try
+
+
+        End Function
+    End Class
+
+
+    <Serializable()> Public Class SynkItemSQLDatabase
+        Public GUID As String = System.Guid.NewGuid.ToString()
+        Public SynkName As String
+        Public Server As String
+        Public Database As String
+        Public Username As String
+        Public Password As String
+        Public Query As String
+        Public FromMidnightMins As Integer
+        Public IntervalMins As Integer
+        Public LastFired As Date
+        Public Overrides Function ToString() As String
+            If SynkName = "" Then SynkName = "(none)"
+            Return SynkName
+        End Function
+
+    End Class
+
+    Dim MySynkItems As New SynkItems
+    Dim ConfigFileName As String = "OasisSynk.config"
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim xSI As New SynkItemSQLDatabase
+        xSI.SynkName = "JailTracker Inmate Sync"
+        xSI.Server = "Jailtracker"
+        xSI.Database = "That awesome databse"
+
+
+        MySynkItems.Add(xSI)
+
+
+        Dim xItem As ListViewItem = lstvSynkList.Items.Add(xSI.SynkName)
+        xItem.Tag = xSI.GUID
+
+
+    End Sub
+
+    Private Sub lstvSynkList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstvSynkList.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub lstvSynkList_KeyDown(sender As Object, e As KeyEventArgs) Handles lstvSynkList.KeyDown
+        Select Case e.KeyCode
+            Case Keys.Delete
+                If MsgBox("Delete Synk?", MsgBoxStyle.YesNo, "Delete") = vbYes Then
+                    For Each xItem In lstvSynkList.SelectedItems
+                        For Each xItem2 In MySynkItems.myList
+                            If xItem2.GUID = xItem.Tag Then
+                                MySynkItems.myList.Remove(xItem2)
+                            End If
+                        Next
+                        lstvSynkList.Items.Remove(xItem)
+                    Next
+                End If
+                MySynkItems.Save(ConfigFileName)
+
+        End Select
+    End Sub
+
+    Private Sub frmOasisSynk_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim SynkCount = MySynkItems.Load(ConfigFileName)
+        For Each xitem In MySynkItems.myList
+            Dim xItemr As ListViewItem = lstvSynkList.Items.Add(xitem.SynkName)
+            xItemr.Tag = xitem.GUID
+        Next
+
+    End Sub
+
+    Private Sub frmOasisSynk_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        MySynkItems.Save(ConfigFileName)
+    End Sub
+End Class
